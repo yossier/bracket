5e2d0b9ed3308a793fec5baeb65d3a7d6c7d7628
Rebecca <raless@umich.edu>
2015-11-10 17:26:16 -0500

    Basic Framework
    Beginning framework but still needs logo

---
+++ OnBoarding.ios.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var Button = require('react-native-button');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+} = React;
+
+
+var bracket = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Bracket
+        </Text>
+       <Button
+        style={styles.buttons}
+        onPress={this._loginPress}>
+        Login
+      </Button>
+      <Button style={styles.buttons} onPress={this._Signup}>
+        Sign Up
+      </Button>
+      </View>
+    );
+  },
+
+  _loginPress(event) {
+    console.log('Pressed!');
+  },
+  _Signup(event){
+    console.log('Sign up pressed');
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E2E2E2',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  pics: {
+    width: 300,
+    height: 200,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#6600FF',
+    marginBottom: 5,
+  },
+  buttons: {
+    borderRadius: 5,
+    height: 50,
+    width: 250,
+    color: 'white',
+    backgroundColor: '#1AA3FF',
+    padding: 15,
+    margin: 8,
+  }
+});
+
+AppRegistry.registerComponent('bracket', () => bracket);
