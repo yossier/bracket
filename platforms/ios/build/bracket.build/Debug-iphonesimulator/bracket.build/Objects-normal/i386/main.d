6a3d13b1a35231df4215d7938576b50e0bdbef4f
Yossef Rosenstein <yossier@587214817.guest.umich.net>
2015-11-20 13:10:26 -0500

    new proj

--- platforms/ios/build/bracket.build/Debug-iphonesimulator/bracket.build/Objects-normal/i386/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: /Users/aaron/Repos/bracket/platforms/ios/internal/main.m \
-  /Users/aaron/Repos/bracket/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/aaron/Repos/bracket/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/aaron/Repos/bracket/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/aaron/Repos/bracket/platforms/ios/internal/TNSDebugging.h
