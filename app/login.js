a08c61f661d8f07ce4233cdf98a5037bea7c5852
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2015-12-03 15:51:55 -0500

    Made login page and started on the serrver

---
+++ app/login.js
@@ -0,0 +1,27 @@
+var observableModule = require("data/observable");
+
+var user = new observableModule.Observable({
+    email: "",
+    password: ""
+});
+
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+var email;
+var password;
+
+exports.loaded = function(args) {
+    var page = args.object;
+    page.bindingContext = user;
+};
+
+exports.signIn = function() {
+    alert("Signing in");
+    console.log(email.text);
+}
+
+exports.register = function() {
+    alert("Registering");
+    var topmost = frameModule.topmost();
+    topmost.navigate("register");    
+}
---
+++ platforms/android/src/main/assets/app/swipe-page.js
@@ -0,0 +1,84 @@
+var sModule = require("./swipe-view-model");
+var dialogs = require("ui/dialogs");
+var gestures = require("ui/gestures");
+var absoluteLayout = require("ui/layouts/absolute-layout");
+var view = require("ui/core/view");
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = sModule.swipeViewModel;
+    var swipeCard = view.getViewById(page, "swipeCard");
+
+    swipeCard.on(gestures.GestureTypes.pan, function (args) {
+		// console.log("Pan deltaX:" + args.deltaX + "; deltaY:" + args.deltaY + ";");
+		if ( typeof this.oldTop === 'undefined' ) {
+	        this.oldTop = swipeCard._oldTop;
+	        this.oldLeft = swipeCard._oldLeft;
+	       	this.panDuration = 0;
+	    }
+
+	    //if the pan is starting from origin then we need to add oldTop and oldLeft
+	    if (panDuration === 0) {
+	    	absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop + args.deltaY);
+	    	absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft + args.deltaX);
+	    }
+	    //this means we are mid-swipe so just use deltaY and deltaX by themselves 
+	    else {
+	    	absoluteLayout.AbsoluteLayout.setTop(swipeCard, args.deltaY);
+	    	absoluteLayout.AbsoluteLayout.setLeft(swipeCard, args.deltaX);
+	    	panDuration++;
+	    }
+
+	    //trying to figure out how to have the card rotate slightly in the swipe direction
+	 	//    swipeCard.animate({
+		//     rotate: args.deltaX/360,
+		//     duration:10
+		// });
+
+
+	   	// if (args.deltaX > 100) {
+	    // 	dialogs.alert("swipe right");
+	    // 	absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop);
+	    //   	absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft);
+	    //   	panDuration = 0;
+	    //   	return;
+	    // } else if (args.deltaX < -90) {
+	    // 	dialogs.alert("swipe left");
+	    // 	absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop);
+	    //   	absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft);
+	    //   	panDuration = 0;
+	    //   	return;
+	    // } 
+
+	    //args.state is undefined for some reason??
+	    if(args.state === gestures.GestureStateTypes.began) {
+	      // Pan began.
+	      // absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop + args.deltaY);
+	      // absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft + args.deltaX);
+	    } else if(args.state === gestures.GestureStateTypes.changed) {
+	      // Pan changed.
+	      // absoluteLayout.AbsoluteLayout.setTop(swipeCard, args.deltaY);
+	      // absoluteLayout.AbsoluteLayout.setLeft(swipeCard, args.deltaX);
+	    } else if(args.state === gestures.GestureStateTypes.ended) {
+	      // Pan ended.
+	      // absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop);
+	      // absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft);
+	      console.log("inside ended");
+	      swipeCard.animate({
+		    translate: { x: -args.deltaX, y: -args.deltaY },
+		    duration: 400
+		  });
+	      return;
+	    } else if(args.state === gestures.GestureStateTypes.cancelled) {
+	      // Pan cancelled.
+	      // absoluteLayout.AbsoluteLayout.setTop(swipeCard, this.oldTop);
+	      // absoluteLayout.AbsoluteLayout.setLeft(swipeCard, this.oldLeft);
+	      console.log("inside cancelled");
+	      swipeCard.animate({
+		    translate: { x: -args.deltaX, y: -args.deltaY },
+		    duration: 400
+		  });
+	      return;
+	    }
+	});
+}
+exports.pageLoaded = pageLoaded;
