def1cd2688c813fcf9c847c55a46f108c21b9481
Rebecca <raless@umich.edu>
2015-12-07 22:39:05 -0500

    Committing

--- platforms/ios/bracket/app/app.js
+++ platforms/ios/bracket/app/app.js
@@ -1,4 +1,13 @@
 var application = require("application");
-application.mainModule = "swipe-page";
+application.mainModule = "main-page";
 application.cssFile = "./app.css";
+
+application.on(application.launchEvent, function (args) {
+    if (args.android) {
+
+    } else if ( args.ios !== undefined ) {
+        //check if logged in and move appropriately
+    }
+});
+
 application.start();
