d3e038d887c443ea55550df8883f8feb8b44a4a6
Aaron Nussbaum <aaron.nussbaum1@gmail.com>
2015-12-10 03:12:08 -0500

    swipe page works, working on index and edge case issues

--- platforms/ios/bracket/app/challenge.js
+++ platforms/ios/bracket/app/challenge.js
@@ -7,44 +7,58 @@ var view = require("ui/core/view");
 // // hide back button
 // navigationItem.setHidesBackButtonAnimated(true, false);
 
-exports.loaded = function(args) {
-  var page = args.object;
-  
-  if (page.ios) {
-    var controller = frameModule.topmost().ios.controller;
-  	// get the view controller navigation item
-	var navigationItem = controller.visibleViewController.navigationItem;
+var topmost = frameModule.topmost();
 
-	// hide back button
-	navigationItem.setHidesBackButtonAnimated(true, false);
-	frameModule.topmost().ios.navBarVisibility = "never";
-
-	//Use this if want to set IOS title bar
-  	// frameModule.topmost().ios.navBarVisibility = "always";
-  	// page.ios.title = "<Choose A Challenge >";
-  }
-  module.exports = {
-  	page: page
-  }
+var navigationEntry = {
+    moduleName: "swipe-page",
+    animated: false
 };
 
+// exports.loaded = function(args) {
+//   var page = args.object;
+  
+//   if (page.ios) {
+//     var controller = frameModule.topmost().ios.controller;
+//   	// get the view controller navigation item
+// 	var navigationItem = controller.visibleViewController.navigationItem;
+
+// 	// hide back button
+// 	// navigationItem.setHidesBackButtonAnimated(true, false);
+// 	// frameModule.topmost().ios.navBarVisibility = "never";
+
+// 	//Use this if want to set IOS title bar
+//   	// frameModule.topmost().ios.navBarVisibility = "always";
+//   	// page.ios.title = "<Choose A Challenge >";
+//   }
+// };
+
 exports.recursion = function(){
-	alert('Recursion Clicked');
-}
+	  // alert('Recursion Clicked');
+    navigationEntry.context = {info: 'recursion'};
+    frameModule.topmost().navigate(navigationEntry);
+};
 
 exports.dataStructures = function(){
-	alert('Data Structures Clicked');
-}
+	  // alert('Data Structures Clicked');
+    navigationEntry.context = {info: "dataStructures"};
+    frameModule.topmost().navigate(navigationEntry);
+};
 
 exports.loops = function(){
-	alert('Loops Clicked');
-}
+	  // alert('Loops Clicked');
+    navigationEntry.context = {info: "loops"};
+    frameModule.topmost().navigate(navigationEntry);
+};
 
 exports.algorithms = function(){
-	alert('Algorithms Clicked');
-}
+	  // alert('Algorithms Clicked');
+    navigationEntry.context = {info: "algorithms"};
+    frameModule.topmost().navigate(navigationEntry);
+};
 
 exports.sorts = function(){
-	alert('Sorts Clicked');
-}
+	  // alert('Sorts Clicked');
+    navigationEntry.context = {info: "sorts"};
+    frameModule.topmost().navigate(navigationEntry);
+};
 
