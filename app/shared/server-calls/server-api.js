3da26f53b2695808e2e1351770cd153b4ce2e053
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2015-12-10 23:42:36 -0500

    added update username route

--- app/shared/server-calls/server-api.js
+++ app/shared/server-calls/server-api.js
@@ -1,6 +1,8 @@
 var config = require("../config");
 var fetchModule = require("fetch");
 
+var jsonHeader = {"Content-Type": "application/json"};
+
 modules.exports = {
     createUser: function(user) {
         return fetchModule.fetch(config.apiUrl + "users", {
@@ -11,9 +13,7 @@ modules.exports = {
                 first_name: user.first_name,
                 last_name: user.last_name
             }),            
-            headers: {
-                "Content-Type": "application/json"
-            }
+            headers: jsonHeader
         })
             .then(function (r) {
                 return r.json();
@@ -39,6 +39,27 @@ modules.exports = {
                 console.log("Error occurred " + e);
                 return null;
             })  
+    },
+
+    updateName: function(user) {
+        return fetchModule.fetch(config.apiUrl + "users/" + user.id + "/name", {
+            method: "PUT",
+            body: JSON.stringify({
+                first_name: user.first_name,
+                last_name: user.last_name,
+                passHash: user.passHash
+            }),
+            header: jsonHeader
+        })
+            .then(function (r) {
+                return r.json();
+            })
+            .then(function (r) {
+                console.log(r);
+                return r
+            }, function (e) {
+                console.log("Error occurred " + e);
+                return null;
+            })
     }
-    
 }
